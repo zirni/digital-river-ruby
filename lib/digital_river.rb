733eea9ac40b3c9e2ac2aa95dfb20c72456c3de5
Matthias Zirnstein <matthias.zirnstein@googlemail.com>
2013-03-18 08:51:44 +0100

    restructure

--- lib/digital_river.rb
+++ lib/digital_river.rb
@@ -1,5 +1,7 @@
 module DigitalRiver
 end
 
-require "digital_river/auth"
 require "digital_river/auth/token"
+require "digital_river/resource"
+require "digital_river/resource/response"
+require "digital_river/auth"
