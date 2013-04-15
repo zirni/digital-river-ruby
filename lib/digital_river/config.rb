b17dd7cdd113cd1e3bcf8d0d75b5bb89d5d3a32a
Matthias Zirnstein <matthias.zirnstein@googlemail.com>
2013-04-15 16:20:14 +0200

    make config host editable

--- lib/digital_river/config.rb
+++ lib/digital_river/config.rb
@@ -1,13 +1,15 @@
 module DigitalRiver
   class Config
-    attr_accessor :url
+    attr_accessor :url, :host
 
     def url
       @url || File.join(host, "v1")
     end
 
+    # host is used to for authentication
+    # it has no versioning in the path
     def host
-      @url || "https://api.digitalriver.com/"
+      @host || "https://api.digitalriver.com/"
     end
   end
 end
