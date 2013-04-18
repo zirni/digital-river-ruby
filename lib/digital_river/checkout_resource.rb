aa5d94339e71eabad2ce198518d9fb453c0b3520
Matthias Zirnstein <matthias.zirnstein@googlemail.com>
2013-04-18 08:19:20 +0200

    add documentation

--- lib/digital_river/checkout_resource.rb
+++ lib/digital_river/checkout_resource.rb
@@ -1,4 +1,19 @@
 module DigitalRiver
+  # A resource to retrieve an url to checkout
+  # the shopping cart on DR servers side
+  #
+  # It returns HTTP status 302 and the checkout url
+  # in the HTTP header attribute location. This location
+  # you can pass to the user.
+  #
+  # For more information check out
+  # https://developers.digitalriver.com/v1/shoppers/web-checkout-resource
+  #
+  # @example
+  #   checkout = CheckoutResource.build(session)
+  #   response = checkout.response
+  #   response.headers["location"] #=> checkout url
+  #   response.status #=> 302
   class CheckoutResource
     def self.build(session)
       new(session)
@@ -7,12 +22,21 @@ module DigitalRiver
     include Resource
     include Resource::Response
 
+    # Specify how to request the resource.
+    # It needs/returns XML instead of json
+    #
+    # @return [DigitalRiver::Response]
+    #
+    # @api private
     def retrieve_response
       token = session.token
       r = Session.build(token, Session.xml_requester(token))
       r.get(url)
     end
 
+    # @return [String]
+    #
+    # @api private
     def url
       File.join(DigitalRiver.config.url, "shoppers/me/carts/active/web-checkout")
     end
--- lib/digital_river/resource/response.rb
+++ lib/digital_river/resource/response.rb
@@ -1,14 +1,32 @@
 module DigitalRiver
   module Resource
+    # Defines the response interface for resources
+    #
+    # @example
+    #   class Sample
+    #     include Response
+    #   end
+    #
+    #   sample = Sample.new
+    #   sample.response
+    #   sample.response!
     module Response
+      # @api private
       def retrieve_response
         session.get(url)
       end
 
+      # Retrieve reponse
+      #
+      # @api public
       def response
         @response ||= retrieve_response
       end
 
+      # Retrieve response and raise exceptions when
+      # the response contains errors
+      #
+      # @api public
       def response!
         return response if !response.errors?
 
