80adee07774689d7badae76571898ae9231e518d
Matthias Zirnstein <matthias.zirnstein@googlemail.com>
2013-04-18 09:02:26 +0200

    add documentation for auth

--- lib/digital_river/auth.rb
+++ lib/digital_river/auth.rb
@@ -1,8 +1,22 @@
 module DigitalRiver
+  # Creates a session by passing a client_id to Digital River.
+  # It will return a access_token we need to authorize all
+  # future API request.
+  #
+  # https://developers.digitalriver.com/apis/list/674/apisuite/oauth-api
+  #
+  # @example
+  #   auth = Auth.new("abc123", :password)
+  #   token = auth.token
+  #   token.access_token #=> long string :)
   class Auth
     include Concord.new(:client_id, :grant_type)
 
-    # It asks the API for a new access token
+    # It asks the API for a new access token.
+    #
+    # @return [Auth::Token]
+    #
+    # @api public
     def token
       response = Request.post(url,
                               :headers => headers,
