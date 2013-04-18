68adf473d3cd18cd4c36ef0a692f40ec77c8035d
Matthias Zirnstein <matthias.zirnstein@googlemail.com>
2013-04-18 08:55:15 +0200

    add documentation for line items resource add

--- lib/digital_river/line_items_resource/add.rb
+++ lib/digital_river/line_items_resource/add.rb
@@ -1,5 +1,13 @@
 module DigitalRiver
   class LineItemsResource
+    # Adds a line item to the current shopping cart.
+    # Pass a product id provided by Digital River.
+    #
+    # https://developers.digitalriver.com/resourcemethod/post-shoppersmecartsactiveline-items
+    #
+    # @example
+    #   add = Add.new(session, {:id => 123})
+    #   response = add.response
     class Add
       include Resource
       include Resource::Response
@@ -15,6 +23,13 @@ module DigitalRiver
         File.join(DigitalRiver.config.url, "shoppers/me/carts/active/line-items")
       end
 
+      # Send product id in the http payload.
+      # With this payload structure, it would be possible to add
+      # more than one product at once
+      #
+      # @return [Hash]
+      #
+      # @api private
       def body
         body = {:lineItems => {:lineItem => [{:product => {:id => options[:id]}}]}}
       end
