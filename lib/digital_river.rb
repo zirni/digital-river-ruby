b6886a11ec5af3abe619b6c9fbba86ecafa63c07
Matthias Zirnstein <matthias.zirnstein@googlemail.com>
2013-04-18 18:48:19 +0200

    delete hashes2ostruct method

--- lib/digital_river.rb
+++ lib/digital_river.rb
@@ -86,19 +86,3 @@ require "digital_river/line_items_resource/destroy"
 require "digital_river/checkout_resource"
 
 require "digital_river/auth"
-
-def hashes2ostruct(object)
-  return case object
-  when Hash
-    object = object.clone
-    object.each do |key, value|
-      object[key] = hashes2ostruct(value)
-    end
-    OpenStruct.new(object)
-  when Array
-    object = object.clone
-    object.map! { |i| hashes2ostruct(i) }
-  else
-    object
-  end
-end
