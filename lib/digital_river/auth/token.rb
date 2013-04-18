bd45287eb3ff621bb8a1b564e688abe7be8cb257
Matthias Zirnstein <matthias.zirnstein@googlemail.com>
2013-04-18 09:08:01 +0200

    sit token into correct project scope

--- lib/digital_river/auth/token.rb
+++ lib/digital_river/auth/token.rb
@@ -1,20 +1,24 @@
 require "adamantium"
 require "anima"
 
-class Token
-  def self.build(attributes)
-    new(attributes.symbolize_keys)
-  end
+module DigitalRiver
+  class Auth
+    class Token
+      def self.build(attributes)
+        new(attributes.symbolize_keys)
+      end
 
-  ATTRIBUTES = [:access_token, :token_type, :expires_in, :refresh_token, :scope].freeze
+      ATTRIBUTES = [:access_token, :token_type, :expires_in, :refresh_token, :scope].freeze
 
-  include Adamantium
-  include Anima.new(*ATTRIBUTES)
+      include Adamantium
+      include Anima.new(*ATTRIBUTES)
 
-  def attributes
-    ATTRIBUTES.inject({}) do |h, name|
-      h[name] = send(name)
-      h
+      def attributes
+        ATTRIBUTES.inject({}) do |h, name|
+          h[name] = send(name)
+          h
+        end
+      end
     end
   end
 end
