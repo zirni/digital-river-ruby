953d77a8824e85f02fd71328b505a1a2c8fb8990
Matthias Zirnstein <matthias.zirnstein@googlemail.com>
2013-04-18 08:44:53 +0200

    add documentation for line items resource all

--- lib/digital_river/line_items_resource/all.rb
+++ lib/digital_river/line_items_resource/all.rb
@@ -1,9 +1,18 @@
 module DigitalRiver
   class LineItemsResource
+    # Get a list of all line items in the current shopping cart.
+    #
+    # For more information check out https://developers.digitalriver.com/resourcemethod/get-shoppersmecartsactiveline-items
+    # @example
+    #   all = All.new(session, {})
+    #   response = all.response
+    #   response.body["lineItems"]["lineItem"] #=> be careful here: it will return an array or a single item. Ensure to explicit cast it to an array in your application.
     class All
       include Resource
       include Resource::Response
       include Concord.new(:session, :options)
+
+      private
 
       def url
         File.join(DigitalRiver.config.url, "shoppers/me/carts/active/line-items")
