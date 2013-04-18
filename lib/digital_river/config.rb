a2b53a6e62d3ea32e8a9b1529a6eb4e979721e68
Matthias Zirnstein <matthias.zirnstein@googlemail.com>
2013-04-18 12:10:06 +0200

    add more doc

--- lib/digital_river/config.rb
+++ lib/digital_river/config.rb
@@ -1,15 +1,37 @@
 module DigitalRiver
   class Config
-    attr_accessor :url, :oauth_url, :host
+    # @api public
+    attr_writer :url
 
+    # @api public
+    attr_writer :oauth_url
+
+    # @api public
+    attr_writer :host
+
+    # The url prefix all resources use to access the API
+    #
+    # @return [String]
+    #
+    # @api public
     def url
       @url || File.join(host, "v1")
     end
 
+    # The url the Auth classes use
+    #
+    # @return [String]
+    #
+    # @api public
     def oauth_url
       @oauth_url || File.join(host, "oauth20/token")
     end
 
+    # The API host end point
+    #
+    # @return [String]
+    #
+    # @api public
     def host
       @host || "https://api.digitalriver.com/"
     end
