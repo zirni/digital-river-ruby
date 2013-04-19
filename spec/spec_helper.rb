a583c4abf3748c99e4a1e8fd4f8ec9f7a565b2fa
Matthias Zirnstein <matthias.zirnstein@googlemail.com>
2013-04-19 17:20:34 +0200

    add simplecov

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,4 +1,7 @@
 # require 'webmock/rspec'
 require "digital_river"
+require "simplecov"
+
+SimpleCov.start
 
 Dir[File.expand_path(File.join(File.dirname(__FILE__),'support','**','*.rb'))].each {|f| require f}
