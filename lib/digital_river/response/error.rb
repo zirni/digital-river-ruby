5fed7360a32ddfca1d592e60b05a9786e4a5ba5d
Matthias Zirnstein <matthias.zirnstein@googlemail.com>
2013-04-22 13:37:28 +0200

    support 3 different error structures

--- lib/digital_river/response/error.rb
+++ lib/digital_river/response/error.rb
@@ -3,18 +3,58 @@ module DigitalRiver
     # Represent an API error
     # https://developers.digitalriver.com/page/common-errors
     class Error < self
+      class NoneNestedStruct
+        def self.transform(error)
+          {"code"        => error["error"],
+           "description" => error["error_description"],
+           "uri"         => error["error_uri"]}
+        end
+      end
+
+      class NestedStruct
+        def self.transform(error)
+          error = error["errors"] if error["errors"]
+          error = error["error"]
+          error = error.first if error.is_a?(Array)
+
+          {"code"        => error["code"],
+           "description" => error["description"],
+           "uri"         => error["relation"]}
+        end
+      end
+
       # Creates an Error instance
+      #
       # @return [Error]
       #
       # @api public
       #
       # @example
-      #   error = Error.build({"errors" => "system-error"}, 500, {"Content-Type" => "text/javascript"})
+      #   error = Error.build({"error" => "system-error", "description" => "Critical system error"}, 500, {"Content-Type" => "text/javascript"})
       #   error.errors? #=> true
       #   error.error_messages #=> system-error
       #   raise error.to_exception
       def self.build(body, status, headers)
-        new(body["errors"], status, headers)
+        error = if body["error"].is_a?(String)
+          NoneNestedStruct.transform(body)
+        elsif body["errors"] || body["error"]
+          NestedStruct.transform(body)
+        else
+          {"code"        => "not given",
+           "description" => "not given",
+           "uri"         => "not given"}
+        end
+
+        new(error, status, headers)
+      end
+
+      # Detects if given information is an error
+      #
+      # @return [Boolean]
+      #
+      # @api public
+      def self.is_error?(body, status, headers)
+        body["errors"] || body["error"]
       end
 
       # Errors indicator
@@ -40,7 +80,7 @@ module DigitalRiver
       #   error = Error.build("payload", 200, {})
       #   error.error_messages
       def error_messages
-        body["error"]
+        body
       end
 
       # Converts the error to an exception
@@ -53,9 +93,7 @@ module DigitalRiver
       #   error = Error.build("payload", 200, {})
       #   error.to_exception
       def to_exception
-        error = body["error"]
-        error = error.first if error.is_a?(Array)
-        BasicError.build(error["code"], error["description"])
+        BasicError.build(body["code"], body["description"])
       end
     end
   end
